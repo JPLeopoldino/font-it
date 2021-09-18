305c04472f736f912dd860cebce6a1443e72b8ff
JPLeopolino <leopoldino26@gmail.com>
2021-09-18 13:22:13 -0300

    feat: project initial configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Icon from 'react-icons-kit';
+import { type } from 'react-icons-kit/feather/type'
+import { bold } from 'react-icons-kit/feather/bold'
+import { italic } from 'react-icons-kit/feather/italic'
+import { code } from 'react-icons-kit/feather/code'
+import { list } from 'react-icons-kit/feather/list'
+import { underline } from 'react-icons-kit/feather/underline'
+import { link2 } from 'react-icons-kit/feather/link2'
+import { quote } from 'react-icons-kit/entypo/quote'
+// import { copy } from 'react-icons-kit/feather/copy'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1 className="title">Font It.</h1>
+      <div className="main-box">
+        <div className="buttons-row">
+          <button className="button">
+            <Icon icon={type} />
+          </button>
+          <button className="button">
+            <Icon icon={bold} />
+          </button>
+          <button className="button">
+            <Icon icon={italic} />
+          </button>
+          <button className="button">
+            <Icon icon={code} />
+          </button>
+          <button className="button">
+            <Icon icon={list} />
+          </button>
+          <button className="button">
+            <Icon icon={underline} />
+          </button>
+          <button className="button">
+            <Icon icon={quote} />
+          </button>
+          <button className="button">
+            <Icon icon={link2} />
+          </button>
+        </div>
+        <textarea className="text-area"/>
+      </div>
     </div>
   );
 }
